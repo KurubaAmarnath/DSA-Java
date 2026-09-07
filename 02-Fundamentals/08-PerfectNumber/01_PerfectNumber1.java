2334008885dec83778ed0b3c80f39f260133e3cb
amar9963 <240332188+amar9963@users.noreply.github.com>
2026-09-07 20:42:22 +0530

    DSA-022 : Check Perfect Number Using Divisor Iteration

---
+++ 02-Fundamentals/08-PerfectNumber/01_PerfectNumber1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class PerfectNumber{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int number = sc.nextInt();
+        int sum = 0;
+        for ( int i = 1; i < number; i++){
+            if ( number % i == 0){
+                sum = sum + i;
+            }
+        }
+        if ( sum == number){
+          System.out.println("Perfect Number");
+        } else{
+            System.out.println("Not a Perfect Number");
+        }
+        sc.close();
+    }
+}
+
+// Time Complexity O(n)
+// Space Complexity O(1)
