0f6d15b7de70bfdcd8ae0635b2dd5d60d4bfe386
KurubaAmarnath <amarnath93811@gmail.com>
2026-09-20 06:35:59 +0530

    DSA-039 : Find Prime Factors using Repeated Division

---
+++ 02-Fundamentals/15-PrimeFactorization/Primefactoriazation01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class PrimeFactorization{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int n = sc.nextInt();
+        System.out.print("Prime Factors :  " );
+        for ( int i =2; i<=n; i++){
+            while ( n % i == 0 ){
+                System.out.print(i + " ");
+                n = n / i;
+            }
+        }
+        sc.close();
+    }
+}
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
